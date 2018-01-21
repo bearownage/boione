c23fd2be00c8257279956ab2960ce1053539f03d
Husain Adam Askari <haskari@ucsc.edu>
2018-01-21 06:58:05 -0800

    working out bugs

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ app.use(bodyParser.json())
 // Index route
 app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
+	mdb.searchMovie({ query: 'Alien' }, (err, res) => {
+  	console.log(res);
+	});
 })
 
 // for Facebook verification
