eef7b804ee64023b7528d80a796d1719a015941b
Husain Adam Askari <haskari@ucsc.edu>
2018-01-20 22:11:35 -0800

    second

--- index.js
+++ index.js
@@ -26,6 +26,28 @@ app.get('/webhook/', function (req, res) {
 	res.send('Error, wrong token')
 })
 
+token = 
+
+app.get('/webhook/', function(req, res) {
+	if (req.query['hub.verify_token'] === "blondiebytes") {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send("Wrong token")
+})
+
+app.post('/webhook/', function(req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0, 100))
+		}
+	}
+	res.sendStatus(200)
+})
+
 // Spin up the server
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
